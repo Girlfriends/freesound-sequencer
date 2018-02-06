fa82b4f093e4a941a9af0e99efd209998123a909
Sam Tarakajian <starakajian@gmail.com>
2018-02-06 08:40:10 -0500

    And what about this?

--- .bin/preinstall.sh
+++ .bin/preinstall.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# GIT_DISCOVERY_ACROSS_FILESYSTEM=1
-# git submodule update --init
+GIT_DISCOVERY_ACROSS_FILESYSTEM=1
+git submodule update --init
 # npm install gulp
 # cd Tone.js/gulp
 # npm install
