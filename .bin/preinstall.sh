e3f80027703994652dd466c2151e6f82347e551c
Sam Tarakajian <starakajian@gmail.com>
2018-02-06 08:57:31 -0500

    Okay, what if I just take that line out?

--- .bin/preinstall.sh
+++ .bin/preinstall.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-GIT_DISCOVERY_ACROSS_FILESYSTEM=1
-git submodule update --init
-# npm install gulp
-# cd Tone.js/gulp
-# npm install
-# cd ..
-# npm run build
-# cd ..
+npm install gulp
+cd Tone.js/gulp
+npm install
+cd ..
+npm run build
+cd ..
