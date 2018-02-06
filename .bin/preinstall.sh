2f195dd97f93e1cb139d034ff0e89023f0376763
Sam Tarakajian <starakajian@gmail.com>
2018-02-06 08:39:07 -0500

    Okay heroku, how do you feel about this?

--- .bin/preinstall.sh
+++ .bin/preinstall.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-GIT_DISCOVERY_ACROSS_FILESYSTEM=1
-git submodule update --init
-npm install gulp
-cd Tone.js/gulp
-npm install
-cd ..
-npm run build
-cd ..
+# GIT_DISCOVERY_ACROSS_FILESYSTEM=1
+# git submodule update --init
+# npm install gulp
+# cd Tone.js/gulp
+# npm install
+# cd ..
+# npm run build
+# cd ..
